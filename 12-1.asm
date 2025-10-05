318646cfd8854bf68043285081fd0f3358e6fdb3
郑阳康 <2126483838@qq.com>
2025-10-05 17:54:39 +0800

    12-4通过offset伪指令实现计算代码段长度的功能

--- 12-1.asm
+++ 12-1.asm
@@ -6,18 +6,12 @@
 ; #################################################################
 
 ;这个程序什么都不用做，仅仅验证12-0.asm中，data段和元数据段的区别，前者会被os覆盖，后者不会。
-assume cs:codesg,ds:datasg
+assume cs:codesg 
 
-datasg segment
-    db 'this is linux, I love you.',0;数据区,另一个进程启动时，不写入数据，查看数据段是不是这个一样的数据就行了。
-datasg ends
-
-codesg segment
-start_:
+codesg segment 
+start_: 
     ;一般来说，开始的1KB是RAM区，即0000:0000-0000:03FF的1024个字节单元
-    ;这个程序什么都不用做，仅仅验证12-0.asm中，data段和元数据段的区别，前者会被os覆盖，后者不会。
-    mov ax, datasg
-    mov ds, ax
+    ;这个程序什么都不用做，仅仅查看ds段和0000:0200的RAM区间内容，验证12-0.asm中，data段和元数据段的区别，前者会被os覆盖，后者不会。
 end_:
      mov ax, 4c00h 
      int 21h 
