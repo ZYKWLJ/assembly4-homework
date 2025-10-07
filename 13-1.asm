556d8d94b1a232b25d74ae66149e6615c044f714
郑阳康 <2126483838@qq.com>
2025-10-07 12:58:14 +0800

    第十二章-内中断和中断程序

--- 13-1.asm
+++ 13-1.asm
@@ -1,27 +1,20 @@
 ; #################################################################
 ; #                                                               #
 ; #  公粽号「专注Linux」，专注Linux内核开发.                         #
-; #  本文链接：https://mp.weixin.qq.com/s/N36H4Ip20RjYfpqTFdn_hA   #    
+; #  本文链接：https://mp.weixin.qq.com/s/CtzjVzj5WpTG8m4nULwNWw   #    
 ; #                                                               #
 ; #################################################################
 
-;验证除法错误中断处理程序。
-assume cs:codesg 
+;验证ss和sp的操作是一体的，不可分割的，Debug不能在中间相应单步中断的。
+assume cs:codesg
 
 codesg segment 
 
 start_: 
-    安装好do0中断处理程序
-    设置好中断向量表, 将do0中断处理程序的入口地址写入0000:0000-0000:0003
-
-end_:
-     mov ax, 4c00h 
-     int 21h 
-
-do0:
-    显示字符串"overflow!";中断处理程序
-    mov ax,4c00h 
-    int 21h
-
+    ; 将栈段寄存器ss设置为0000h，栈指针sp设置为0000h，也就是将栈顶设置在0000:0000处。
+    mov ax,0000h
+    mov ss,ax
+    mov sp,0000h
+    
 codesg ends
 end start_
